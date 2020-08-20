b61448944fa582d537143a2610f03bb3ac0db5ff
aleksandr-tyagun <alexander.tiagun@gmail.com>
2020-08-20 18:15:15 +0300

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,57 +1,74 @@
-import React, { useState } from "react";
-import { useScrollPosition } from "@n8tb1t/use-scroll-position";
-import { InView } from "react-intersection-observer";
+import React, { useRef } from "react";
+import { useScrollPosition } from '@n8tb1t/use-scroll-position'
+import { useInView } from 'react-intersection-observer'
 
 import "./App.scss";
 
 const settings = {
   delay: "3s",
   ease: "cubic-bezier(0.16, 1, 0.3, 1)",
-  divider: 4,
 };
 
-const THRESHOLD = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
+function thresholdList() {
+  let thresholds = [];
+  let numSteps = 20;
+
+  for (let i=1.0; i<=numSteps; i++) {
+    let ratio = i/numSteps;
+    thresholds.push(ratio);
+  }
+
+  thresholds.push(0);
+  return thresholds;
+}
 
 function App() {
-  const [style, setStyle] = useState();
-  const [elementStyle40, setElementStyle40] = useState();
-  const [elementStyle80, setElementStyle80] = useState();
+  const scrollY = useRef(0);
+  const distanceToTop = useRef(0);
+  const elRef = useRef(null);
 
-  useScrollPosition(
-    ({ prevPos, currPos }) => {
-      const style = {
-        transform: `translate3d(0, ${currPos.y / settings.divider}px, 0)`,
-        transition: `transform ${settings.delay} ${settings.ease}`,
-      };
-      setStyle(style);
-    },
-    null,
-    null,
-    false,
-    300
-  );
+  const visibleFlag = useRef(false);
+  const startPosition = useRef(0);
 
-  const handleInView = (inView, entry) => {
-    const translateY = (base, entry) => {
-      return base - entry.intersectionRatio * base;
-    };
+  const [blockRef, inView, entry] = useInView({
+    threshold: thresholdList(),
+  })
 
-    const style40 = {
-      transform: `translate3d(0, ${translateY(40, entry)}px, 0)`,
-      transition: `transform ${settings.delay} ${settings.ease}`,
-    };
-    const style80 = {
-      transform: `translate3d(0, ${translateY(80, entry)}px, 0)`,
-      transition: `transform ${settings.delay} ${settings.ease}`,
-    };
+  const getCoef = (distToTop, startPos) => {
+    const result = distToTop / startPos;
 
-    setElementStyle40(style40);
-    setElementStyle80(style80);
-  };
+    if(!startPos || result < 0) {
+      return 0
+    }
+
+    if(result > 1) {
+      return 1
+    }
+    
+    return result;
+  }
+
+  useScrollPosition(({ prevPos, currPos }) => {
+    if(inView) {
+      scrollY.current = currPos.y
+      distanceToTop.current = elRef.current.getBoundingClientRect().top;
+      // console.log('\n');
+      // console.log("Distance to top: ", distanceToTop.current);
+      // console.log("scroll Y: ", scrollY.current);
+      // console.log("start position", startPosition.current);
+
+      console.log(getCoef(distanceToTop.current, startPosition.current))
+    }
+
+    if(visibleFlag.current === false && inView) {
+      visibleFlag.current = true;
+      startPosition.current = distanceToTop.current;
+    }
+  })
 
   return (
     <div className="App">
-      <div className="container" style={style}>
+      <div className="container">
         <div className="article">
           <div className="element">
             <div className="element__header">
@@ -66,25 +83,23 @@ function App() {
             </div>
           </div>
         </div>
-        <InView
-          as="div"
-          className="article"
-          threshold={THRESHOLD}
-          onChange={handleInView}
-        >
-          <div className="element">
+        <div ref={blockRef} className="article">
+          <div ref={elRef} className="element">
             <div className="element__header">
               <p>malesuada pellentesque</p>
             </div>
             <div className="element__text">
-              <p style={elementStyle40}>Lorem ipsum dolor sit amet</p>
-              <p style={elementStyle40}>consectetur adipiscing elit</p>
+              {/* <p style={elementStyle40}>Lorem ipsum dolor sit amet</p>
+              <p style={elementStyle40}>consectetur adipiscing elit</p> */}
+              <p>Lorem ipsum dolor sit amet</p>
+              <p>consectetur adipiscing elit</p>
             </div>
             <div className="element__tags">
-              <p style={elementStyle80}>ipsum, dolor, sit</p>
+              {/* <p style={elementStyle80}>ipsum, dolor, sit</p> */}
+              <p>ipsum, dolor, sit</p>
             </div>
           </div>
-        </InView>
+        </div>
         <div className="article">
           <div className="element">
             <div className="element__header">
